c0561ce406902f968c0f7ed4e15754d59a817f5b
Tomoyuki HIRAI <gotetu@gmail.com>
2020-06-19 21:54:19 +0900

    tsdoc形式によるヘッダコメントの追加

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -12,17 +12,37 @@ export class CartService {
     private http: HttpClient
   ) { }
 
+  /**
+   * 商品をカートに追加する.
+   * 
+   * @param product 商品
+   */
   addToCart(product) {
     this.items.push(product);
   }
+
+  /**
+   * カートに追加されている商品を取得する.
+   * 
+   * @returns 商品の配列
+   */
   getItems() {
     return this.items;
   }
+
+  /**
+   * カートをクリアする.
+   */
   clarCart() {
     this.items = [];
     return this.items;
   }
 
+  /**
+   * 配送料を取得する
+   * 
+   * @return 配送料オブジェクト
+   */
   getShippingPrices(){
     return this.http.get('/assets/shipping.json');
   }
