dd4e65120eb2d9970db5b1b02f856df0feb98ad2
Tomoyuki HIRAI <gotetu@gmail.com>
2020-06-22 23:04:16 +0900

    テスト項目の改善

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,7 +29,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tutorial app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('router-defined')).toBeDefined();
   });
 });
