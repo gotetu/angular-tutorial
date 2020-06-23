f567530333dc79dcee4dfa05d746100693df8fe1
Tomoyuki HIRAI <gotetu@gmail.com>
2020-06-24 00:54:34 +0900

    テスト項目の追加

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -16,4 +16,17 @@ describe('CartService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('カートに設定した商品を取り出す', () => {
+    service.addToCart({name: 'name', price: 199, description: 'description'});
+    expect(service.getItems()).toEqual([{name: 'name', price: 199, description: 'description'}]);
+  });
+  it('カートに設定した空データを取り出す', () => {
+    service.addToCart({name: '', price: 0, description: ''});
+    expect(service.getItems()).toEqual([{name: '', price: 0, description: ''}]);
+  });
+  it('カートに設定した不正な値付け商品を取り出す', () => {
+    service.addToCart({name: '意味のない商品', price: -1, description: '不正な値付け'});
+    expect(service.getItems()).toEqual([{name: '意味のない商品', price: -1, description: '不正な値付け'}]);
+  });
 });
